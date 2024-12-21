ee37f22fe0035b69b7fb55e84df77e2640f3b94a
Michael Fleming <mikahl171@gmail.com>
2024-12-21 17:18:24 -0600

    css updates. More coming. Needed a stopping point

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Project from '../components/Project';
-import './Portfolio.css';
 
 const Portfolio = () => {
   const projects = [
@@ -8,26 +7,24 @@ const Portfolio = () => {
       title: 'Project 1',
       image: '/path-to-image1.jpg',
       liveLink: 'https://example.com/project1',
-      repoLink: 'https://github.com/raychorn/project1',
+      repoLink: 'https://github.com/yourusername/project1',
+      bubbleStyle: 'speech', // Ensure the "speech" style is used
     },
     {
       title: 'Project 2',
       image: '/path-to-image2.jpg',
       liveLink: 'https://example.com/project2',
-      repoLink: 'https://github.com/raychorn/project2',
+      repoLink: 'https://github.com/yourusername/project2',
+      bubbleStyle: 'speech', // Apply the "speech" style here as well
     },
-    // Add more projects here
   ];
 
   return (
-    <section className='portfolio'>
-      <h2>Portfolio</h2>
-      <div className='projects'>
-        {projects.map((project, index) => (
-          <Project key={index} {...project} />
-        ))}
-      </div>
-    </section>
+    <div className="projects">
+      {projects.map((project, index) => (
+        <Project key={index} {...project} />
+      ))}
+    </div>
   );
 };
 
