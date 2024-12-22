30f3006d1944a50c0429ec58bbe0eca1c1396800
Michael Fleming <mikahl171@gmail.com>
2024-12-22 00:41:11 -0600

    pushing everything. Too many files touched

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,16 +4,10 @@ import './About.css';
 const About = () => {
   return (
     <section className="about">
-      <img
-        src="/assets/profile-pic.jpg" // Replace with your image path
-        alt="Your Name"
-        className="profile-pic"
-      />
+      <h1>Michael Fleming</h1>
       <h2>About Me</h2>
       <p>
-        Hi, I'm [Your Name], a passionate web developer with experience in
-        creating responsive, user-friendly web applications using React, JavaScript,
-        and modern front-end technologies.
+        Hi, I'm Michael Fleming a passionate, yet new, Full Stack developer with recently gained knowledge in PERN and MERN stack development using techologies like like React, Node.js, Express, and MongoDB. I am a recent graduate of Southern Methodist University's Full Stack Development Bootcamp. I am excited to continue learning and growing as a developer.
       </p>
     </section>
   );
