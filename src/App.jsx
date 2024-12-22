e4c1c4213bc4ee508b8ba06fe6b0920e61dca9a5
Michael Fleming <mikahl171@gmail.com>
2024-12-22 15:50:11 -0600

    added emailjs for contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer"; // Import Footer
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
@@ -9,13 +10,18 @@ import Resume from "./pages/Resume";
 const App = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={<Resume />} />
-      </Routes>
+      <div className="app">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/resume" element={<Resume />} />
+          </Routes>
+        </main>
+        <Footer /> {/* Footer is now displayed on every page */}
+      </div>
     </Router>
   );
 };
